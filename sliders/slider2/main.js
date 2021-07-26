9afc2c0c7418ac04c79468a39456b7629a412f4b
zorexweb <87967148+zorexweb@users.noreply.github.com>
2021-07-26 14:17:58 +0600

    Update main.js

--- sliders/slider2/main.js
+++ sliders/slider2/main.js
@@ -1,67 +1,104 @@
 (function ($) {
 	"use strict";
-
+ 
     jQuery(document).ready(function($){
 
-	    
-     	$(".homepage-slides").owlCarousel({
-    		items: 1,
-    		nav: true,
-    		dots: true,
-    		loop: true,
-    		autoplay: true,
-           navText: ["<i class='fa fa-angle-left'</i>", "<i class='fa fa-angle-right'</i>"],
-    	});
-        
-         
-     	$(".homepage-slides").on("translate.owl.carousel", function(){
-			$(".home-silde-contant h1").removeClass("animated fadeInLeft")
-			$(".home-silde-contant p").removeClass("animated fadeInRight")
-    		
-    	});
-        
-			  
-     	$(".homepage-slides").on("translated.owl.carousel", function(){
-			$(".home-silde-contant h1").addClass("animated fadeInLeft")
-			$(".home-silde-contant p").addClass("animated fadeInRight")
-    		
-    	});
-        
-        
-        
-		
-		
-		$(".video-icon").magnificPopup({
-         type: 'video',
-       
-       });
-		
-		
-   
-		
-		 new WOW().init();
 
-        
-        $(window).scroll(function(){
+        $(document).ready(function(){
+
+		    $(window).scroll(function(){
 		        if ($(this).scrollTop() > 100) {
-		            $('.scrollToTop').fadeIn();
+		            $('.TopUP').fadeIn();
 		        } else {
-		            $('.scrollToTop').fadeOut();
+		            $('.TopUP').fadeOut();
 		        }
 		    });
 
 		    //Click event to scroll to top
-		    $('.scrollToTop').on('click', function(){
+		    $('.TopUP').on('click', function(){
 		        $('html, body').animate({scrollTop : 0},800);
 		        return false;
 		    });
 
-        
+		    $(document).ready(function(){
+				$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+					event.preventDefault(); 
+					event.stopPropagation(); 
+					$(this).parent().siblings().removeClass('open');
+					$(this).parent().toggleClass('open');
+				});
+			});
+
+		});
+
+
+
+		
+
+		$(".home_slider_sec").owlCarousel({
+		    dots: false,
+			autoplay: true,
+			margin: 20,
+			loop: true,
+			nav: true,
+			navText: [
+				"<i class='fa fa-angle-left'></i>",
+				"<i class='fa fa-angle-right'></i>"
+			],
+		    responsiveClass: true,
+		    responsive: {
+		        0:{
+		          items: 1
+		        },
+		        480:{
+		          items: 1
+		        },
+		        768:{
+		          items: 1 
+		        },
+		        800:{
+		          items: 1
+		        }
+		    }
+		});
+ 
+
+		$(".clint_sec").owlCarousel({
+		    dots: false,
+			autoplay: true,
+			margin: 15,
+			loop: true,
+			nav: true,
+			navText: [
+				"<i class='fa fa-angle-left'></i>",
+				"<i class='fa fa-angle-right'></i>"
+			],
+		    responsiveClass: true,
+		    responsive: {
+		        0:{
+		          items: 1
+		        },
+		        480:{
+		          items: 2
+		        },
+		        768:{
+		          items: 4 
+		        },
+		        800:{
+		          items: 5
+		        }
+		    }
+		});
+
+
 
     });
 
 
+    jQuery(window).load(function(){
+
+        
+    });
 
 
-
-}(jQuery));	
+}(jQuery));
